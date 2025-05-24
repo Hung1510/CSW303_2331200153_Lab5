0cf70677aa926d40574ff4fb68aa6b66ca5116f0
Phạm Trần Gia Hưng <giahung1510200519@gmail.com>
2025-05-24 08:37:46 +0700

    Delete src directory

--- src/Contract.java
+++
@@ -1,7 +0,0 @@
-public interface Contract{
-    void BuildContractId(String contractId);
-    void BuildPropertyId(String propertyId);
-    void BuildTenantId(String tenantId);
-    void BuildRentAmount(double rentAmount);
-    void SignContract(String Contract);       
-}
